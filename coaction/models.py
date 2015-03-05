f2d5b931b8c0bf090516b86d72f1039e13a9fd68
Gordon Fierce <gordon.fierce@gmail.com>
2015-03-05 10:11:47 -0500

    Start task model.

--- coaction/models.py
+++ coaction/models.py
@@ -1,8 +1,12 @@
 from .extensions import db
 
-"""Add your models here."""
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
