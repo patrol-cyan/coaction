db6a4ee98784d26304005c54588690c66a574655
Gordon Fierce <gordon.fierce@gmail.com>
2015-03-05 09:02:59 -0500

    Start User model.

--- coaction/models.py
+++ coaction/models.py
@@ -1,3 +1,8 @@
 from .extensions import db
 
 """Add your models here."""
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
