cd9e988d1211462ea3d7abc0227f625161333e2e
Gordon Fierce <gordon.fierce@gmail.com>
2015-03-08 16:27:50 -0400

    Remove unneccessary whitespace.

--- coaction/models.py
+++ coaction/models.py
@@ -2,7 +2,6 @@ from .extensions import db, bcrypt
 from marshmallow import Schema, fields, ValidationError
 from flask.ext.login import UserMixin
 
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
